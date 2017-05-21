93a77abc4a16ad5dfe5ea5cf300af235d8777e20
Johnny <johnnyhuynhdev@gmail.com>
2017-05-21 14:44:20 +1000

    Added remove/edit business time

--- public/js/app.js
+++ public/js/app.js
@@ -11215,8 +11215,51 @@ __webpack_require__(29);
 //     el: '#app'
 // });
 
+// Source: https://www.snip2code.com/Snippet/534282/Laravel-5---Use-anchor-links-for-DELETE-
+// Modified to use data-method attribute
+var anchorMethod = {
+    init: function init() {
+        $('a[data-method]').on('click', function (e) {
+            e.preventDefault();
+            var link = $(this);
+            var httpMethod = link.data('method').toUpperCase();
+            var form;
+
+            if ($.inArray(httpMethod, ['PUT', 'DELETE']) === -1) {
+                return;
+            }
+
+            anchorMethod.submit(link);
+        });
+    },
+
+    submit: function submit(link) {
+        var form = $('<form>', {
+            'method': 'POST',
+            'action': link.attr('href')
+        });
+
+        var token = $('<input>', {
+            'type': 'hidden',
+            'name': '_token',
+            'value': $('meta[name="csrf-token"]').attr('content')
+        });
+
+        var hiddenInput = $('<input>', {
+            'name': '_method',
+            'type': 'hidden',
+            'value': link.data('method')
+        });
+
+        form.append(token, hiddenInput).appendTo('body').submit();
+    }
+};
+
 $(document).ready(function () {
-  $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // Enable anchor method to create PUT DELETE anchor tags
+    anchorMethod.init();
 });
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2)))
 
